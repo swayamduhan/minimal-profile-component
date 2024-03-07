deee92d53922541d82c56b1276b35702fc0b9df0
Swayam Duhan <workplace.swayam@gmail.com>
2024-03-07 16:08:24 +0530

    changed css

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,5 @@
 export function Input({ setName, setAge, setPlace }){
-    return <div>
+    return <div className="input-container">
         <input className="input-box" type="text" placeholder="enter name" onChange={e => setName(e.target.value)}/>
         <input className="input-box" type="text" placeholder="enter age" onChange={e => setAge(e.target.value)}/>
         <input className="input-box" type="text" placeholder="enter place" onChange={e => setPlace(e.target.value)}/>
